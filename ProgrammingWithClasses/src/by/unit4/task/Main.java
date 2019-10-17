a43b1a15c07ab9c36ae5f228438adf95232f5c7f
AnnaKotov <anna.kotova@gmail.com>
2019-10-17 11:14:47 +0300

    latest vers

--- ProgrammingWithClasses/src/by/unit4/task/Main.java
+++ ProgrammingWithClasses/src/by/unit4/task/Main.java
@@ -1,13 +1,22 @@
 package by.unit4.task;
 
 import by.unit4.task.t1.Test1;
+import by.unit4.task.t2.Test2;
 
 public class Main {
     public static void main (String args[]) {
-        Test1 t = new Test1(4, 5);
-        System.out.println(t.sumUp());
-        System.out.println(t.isBigger());
-        t.printTest1();
+
+        // check class Test1
+
+        Test1 t1 = new Test1(4, 5);
+        System.out.println(t1.sumUp());
+        System.out.println(t1.isBigger());
+        t1.printTest1();
+
+        // check class Test 2
+
+        Test2 t2 = new Test2();
+        Test2 t3 = new Test2(4,5);
     }
 
 }
---
+++ ProgrammingWithClasses/src/by/unit4/task/t2/Test2.java
@@ -0,0 +1,32 @@
+package by.unit4.task.t2;
+
+public class Test2 {
+    int var1;
+    int var2;
+
+    public Test2(int var1, int var2) {
+        this.var1 = var1;
+        this.var2 = var2;
+    }
+
+    public Test2() {
+        var1 = 1;
+        var2 = 1;
+    }
+
+    public int getVar1() {
+        return var1;
+    }
+
+    public void setVar1(int var1) {
+        this.var1 = var1;
+    }
+
+    public int getVar2() {
+        return var2;
+    }
+
+    public void setVar2(int var2) {
+        this.var2 = var2;
+    }
+}
